d6d2a8fa5c98af1ee8a1db678bf471a7e253f1d3
DataDelft <vincent@datadelft.nl>
2021-03-10 12:50:30 +0100

    mouse input
    added mouselook

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,25 +7,33 @@ public class PlayerMovement : NetworkedBehaviour  // use networkedbehavior, that
 {
     //declare vars
     CharacterController cc;
+    float pitch = 0f;
+    public Transform camTransform;
     public float speed = 5f;
     public float MouseSensitivityX = 3f;
     public float MouseSensitivityY = 3f;
-
+    
     void Start()
     {
-        cc = GetComponent<CharacterController>(); // get the charactercontroller reference
+        if (!IsLocalPlayer) // If we are not the local player
+        {
+            camTransform.GetComponent<AudioListener>().enabled = false;
+            camTransform.GetComponent<Camera>().enabled = false;
+        }
+        else // if we are the local player
+        {
+            cc = GetComponent<CharacterController>(); // get the charactercontroller reference
+        }
+
     }
 
 
     private void FixedUpdate()
     {
-        if (!IsLocalPlayer) // If we are not the local player
-        { 
-
-        }
-        else // if we are the local player
+        if (IsLocalPlayer) // if we are the local player
         {
-            movePlayer();
+            movePlayer(); // do movement
+            mouseLook();  // do mouse looking
         }
         
     }
@@ -34,15 +42,17 @@ public class PlayerMovement : NetworkedBehaviour  // use networkedbehavior, that
     {
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")); // get user input
         move = Vector3.ClampMagnitude(move, 1f); // clamp the value to a maximum of 1, normalising the diagonal movement inputs
-        move = transform
-        cc.SimpleMove(move * speed); // do the actual movement
+        move = transform.TransformDirection(move); // set the direction of the movement to match the transform direction that was set by the mouseLook function
+        cc.SimpleMove(move * speed); // do the actual movement trough the character controler
 
     }
 
     void mouseLook()
     {
-        float mouseX = Input.GetAxis("Mouse X") * MouseSensitivityX;
-        transform.Rotate(0, mouseX, 0);
-
+        float mouseX = Input.GetAxis("Mouse X") * MouseSensitivityX; // get horizontal mouse movement with sensitivity
+        transform.Rotate(0, mouseX, 0); // rotate the playerObject
+        pitch -= Input.GetAxis("Mouse Y") * MouseSensitivityY; // get the vertical mouse movement
+        pitch = Mathf.Clamp(pitch, -45f, 45f); // clamp to maximum viewing angle
+        camTransform.localRotation = Quaternion.Euler(pitch, 0, 0); // Set the camerapitch accordingly
     }
 }
